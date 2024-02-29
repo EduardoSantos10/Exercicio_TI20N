262624e3bec098f5383cf580e144ae8ccc3d5ad9
EDUARDO SANTOS DA SILVA <eduardo.ssilva36@senacsp.edu.br>
2024-02-28 22:17:17 -0300

    Primeiro Commit

---
+++ Exercicios TI20N/Model.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_TI20N
+{
+    class Model
+    {
+    
+        //Construtor
+        public Model() 
+        {
+        
+        
+        
+        
+        }//Fim do Construtor 
+
+        //Exercício 01: Faça um programa que imprima os números de 1 a 10.
+        public string ExercicioUm() 
+        {
+
+
+
+
+            string resultado = "";
+
+
+            for (int i = 0; i < 10; i++) 
+            {
+
+                resultado += "\n" + i+1;
+            
+            
+            
+            }
+            return resultado;
+        }//Fim do Método
+    
+    
+    }//Fim da Classe
+
+}//Fim do Projeto
---
+++ Exercicios TI20N/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_TI20N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }//Fim do Main
+    }//Fim da Classe
+}// Fim do Projeto
